894b0b16524dab6a3ab03496b6d24776b432ec3b
ryan123456789-dotcom <nhienvo29@ssis.edu.vn>
2026-05-21 08:58:58 +0700

    Update picture.py
    Code for yellow triangle

--- picture.py
+++ picture.py
@@ -30,7 +30,11 @@ def draw_picture(width, height):
     
     
     # code for YELLOW triangles
-   
+    sg.set_fill_color("yellow") # set fill color to yellow
+    
+    for i in range(5):   # fill_triangle(x1, y1, x2, y2, x3, y3)
+        sg.fill_triangle(1*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 2*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 2*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT) 
+
    
     # code for GREEN triangles
  
